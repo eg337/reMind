106f3b829da952406b8133701276c235fa6a05fa
eg337 <68549574+eg337@users.noreply.github.com>
2024-02-09 02:15:39 -0500

    Update urls.py added comma

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include # 👈 Add include here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
 
 ]
